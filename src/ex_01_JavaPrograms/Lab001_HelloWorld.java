afa75aa961006f46589d0e80846b2ce20403aacb
deepqa963 <deepika.mukherjee@gmail.com>
2025-09-10 23:21:51 +0530

    After email id update

--- src/ex_01_JavaPrograms/Lab001_HelloWorld.java
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -5,10 +5,7 @@ public class Lab001_HelloWorld {
         public static void main(String[] args) {
         System.out.println("Hello World");
             System.out.println("Hello World new");
-            System.out.println("Hello World new");
-            System.out.println("Hello World new33");
-            System.out.println("New Hello World new");
-            System.out.println("New Hello World new");
+
 
     }
 }
