31f92f31fe18444c776e2715c56ba5be3b5af4d5
deepqa963 <deepqa963@gmail.com>
2025-09-10 23:11:12 +0530

    Third trial commit..

--- src/ex_01_JavaPrograms/Lab001_HelloWorld.java
+++ src/ex_01_JavaPrograms/Lab001_HelloWorld.java
@@ -8,6 +8,7 @@ public class Lab001_HelloWorld {
             System.out.println("Hello World new");
             System.out.println("Hello World new33");
             System.out.println("New Hello World new");
+            System.out.println("New Hello World new");
 
     }
 }
