08c188ca68a0689eccfa419306977abe5a633fbf
deepqa963 <deepika.mukherjee@gmail.com>
2025-10-25 10:46:10 +0530

    Ice breaker

---
+++ src/ex_01_JavaPrograms/Icebreakerclass.java
@@ -0,0 +1,8 @@
+package ex_01_JavaPrograms;
+
+public class Icebreakerclass {
+    public static void main(String[] args) {
+        System.out.println("I am doing it..I am an Automation Tester");
+
+    }
+}
